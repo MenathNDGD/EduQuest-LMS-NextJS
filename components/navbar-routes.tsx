780530a4de58ab0b1d8175e7fcb037d101e1abf5
Menath Nadungoda <nadungmenath1@gmail.com>
2024-07-05 23:47:00 +0530

    added navbar routes

---
+++ components/navbar-routes.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { UserButton } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
+
+export const NavbarRoutes = () => {
+
+    const pathname = usePathname();
+
+    const isTeacherPage = pathname?.startsWith("/teacher");
+    const isPlayerPage = pathname?.includes("/chapter");
+
+    return (
+        <div className="flex justify-end gap-x-2 w-full">
+            {isTeacherPage || isPlayerPage ? (
+                <Link href="/">
+                    <Button size="sm">
+                        <LogOut className="h-4 w-4 mr-2" />
+                        Exit
+                    </Button>
+                </Link>
+            ) : (
+                <Link href="/teacher/courses">
+                    <Button size="sm">
+                        Teacher Mode
+                    </Button>
+                </Link>
+            )}
+            <UserButton 
+                afterSignOutUrl="/" 
+            />
+        </div>
+    );
+}
