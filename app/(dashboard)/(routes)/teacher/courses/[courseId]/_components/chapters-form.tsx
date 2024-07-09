5f17b38a7e1481eee3bb6fa70f9caf3a9d9e0c04
Menath Nadungoda <nadungmenath1@gmail.com>
2024-07-09 20:41:54 +0530

    create routes for chapter uploading

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -19,7 +19,9 @@ import {
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, PlusCircle } from "lucide-react";
+import { ChaptersList } from "./chapters-list";
+
 import { cn } from "@/lib/utils";
 
 interface ChaptersFormProps {
@@ -64,8 +66,33 @@ export const ChaptersForm = ({
         }
     }
 
+    const onReorder = async (updateDate: { id: string; position: number }[]) => {
+        try {
+            setIsUpdating(true);
+
+            await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+                list: updateDate
+            });
+            toast.success("Chapters Reordered");
+            router.refresh();
+        } catch {
+            toast.error("Something Went Wrong");
+        } finally {
+            setIsUpdating(false);
+        }
+    }
+
+    const onEdit = (id: string) => {
+        router.push(`/teacher/courses/${courseId}/chapters/${id}`);
+    }
+
     return (
-        <div className="mt-6 border bg-slate-100 rounded-md p-4">
+        <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
+            {isUpdating && (
+                <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-md flex items-center justify-center">
+                    <Loader2 className="animate-spin h-6 w-6 text-sky-600" />
+                </div>
+            )}
             <div className="font-medium flex items-center justify-between">
                 Course Chapters
                 <Button onClick={toggleCreating}>
@@ -116,7 +143,11 @@ export const ChaptersForm = ({
                     !initialData.chapters.length && "text-slate-500 italic"
                 )}>
                     {!initialData.chapters.length && "No Chapters"}
-                    {/*TODO: Add a list of chapters*/}
+                    <ChaptersList 
+                        onEdit={onEdit}
+                        onReorder={onReorder}
+                        items={initialData.chapters || []}
+                    />
                 </div>
             )}
             {!isCreating && (
