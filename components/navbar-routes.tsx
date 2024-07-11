252907ddca2b741102d1a9d3d7610cd38a82029d
Menath Nadungoda <nadungmenath1@gmail.com>
2024-07-12 00:54:06 +0530

    created the navbar route component

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -3,8 +3,9 @@
 import { UserButton } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
-import { LogOut } from "lucide-react";
+import { LogOut, UserIcon } from "lucide-react";
 import Link from "next/link";
+import { SearchInput } from "./search-input";
 
 export const NavbarRoutes = () => {
 
@@ -12,8 +13,15 @@ export const NavbarRoutes = () => {
 
     const isTeacherPage = pathname?.startsWith("/teacher");
     const isPlayerPage = pathname?.includes("/chapter");
+    const isSearchPage = pathname === '/search';
 
     return (
+        <>
+        {isSearchPage && (
+            <div className="hidden md:block">
+                <SearchInput />
+            </div>
+        )}
         <div className="flex justify-end gap-x-2 w-full">
             {isTeacherPage || isPlayerPage ? (
                 <Link href="/">
@@ -25,6 +33,7 @@ export const NavbarRoutes = () => {
             ) : (
                 <Link href="/teacher/courses">
                     <Button size="sm">
+                        <UserIcon className="h-4 w-4 mr-2" />
                         Teacher Mode
                     </Button>
                 </Link>
@@ -33,5 +42,6 @@ export const NavbarRoutes = () => {
                 afterSignOutUrl="/" 
             />
         </div>
+        </>
     );
 }
