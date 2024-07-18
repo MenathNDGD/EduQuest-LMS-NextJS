6806f7a6b5780b7f2f8719d2e3d7a0e37a65102f
Menath Nadungoda <nadungmenath1@gmail.com>
2024-07-19 00:38:03 +0530

    modified the navbar routes with new contents

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { useAuth, UserButton } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
-import { Button } from "@/components/ui/button";
 import { LogOut, UserIcon } from "lucide-react";
 import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { isTeacher } from "@/lib/teacher";
+
 import { SearchInput } from "./search-input";
 
 export const NavbarRoutes = () => {
-
+    const { userId } = useAuth();
     const pathname = usePathname();
 
     const isTeacherPage = pathname?.startsWith("/teacher");
@@ -30,14 +33,14 @@ export const NavbarRoutes = () => {
                         Exit
                     </Button>
                 </Link>
-            ) : (
+            ) : isTeacher(userId) ? (
                 <Link href="/teacher/courses">
                     <Button size="sm">
                         <UserIcon className="h-4 w-4 mr-2" />
                         Teacher Mode
                     </Button>
                 </Link>
-            )}
+            ): null}
             <UserButton 
                 afterSignOutUrl="/" 
             />
