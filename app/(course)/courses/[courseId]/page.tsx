001745e933f4cee1742e3c29042e9c62ac52bab1
Menath Nadungoda <nadungmenath1@gmail.com>
2024-07-19 00:32:32 +0530

    added authorized user access to course creation

--- app/(course)/courses/[courseId]/page.tsx
+++ app/(course)/courses/[courseId]/page.tsx
@@ -1,13 +1,32 @@
-const CourseIdPage = ({
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+const CourseIdPage = async ({
     params
 }: {
     params: { courseId: string; }
 }) => {
-    return ( 
-        <div>
-            Watch the course
-        </div>
-     );
+    const course = await db.course.findUnique({
+        where: {
+            id: params.courseId,
+        },
+        include: {
+            chapters: {
+                where: {
+                    isPublished: true,
+                },
+                orderBy: {
+                    position: "asc"
+                }
+            }
+        }
+    });
+
+    if (!course) {
+        return redirect("/");
+    }
+
+    return redirect(`/courses/${course.id}/chapters/${course.chapters[0].id}`);
 }
  
 export default CourseIdPage;
