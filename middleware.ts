a759beaec17e2bb2ffc356030c8a0cf9e3ee1d4f
Menath Nadungoda <nadungmenath1@gmail.com>
2024-09-24 10:40:49 +0530

    modified the middleware authentication

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,17 @@
+import { NextResponse, NextRequest, NextFetchEvent } from "next/server";
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  publicRoutes: ["/api/webhook"]
-});
+const PUBLIC_ROUTES = ["/api/webhook"];
+
+export default function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+
+  if (PUBLIC_ROUTES.includes(pathname)) {
+    return NextResponse.next();
+  }
+
+  return clerkMiddleware()(req, {} as NextFetchEvent);
+}
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
