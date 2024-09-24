15f7d5f05c7e36c971d8d31f909a00d58c40ef42
Menath Nadungoda <nadungmenath1@gmail.com>
2024-09-24 09:48:08 +0530

    modified the navbar routes component

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -11,40 +11,38 @@ import { isTeacher } from "@/lib/teacher";
 import { SearchInput } from "./search-input";
 
 export const NavbarRoutes = () => {
-    const { userId } = useAuth();
-    const pathname = usePathname();
+  const { userId } = useAuth();
+  const pathname = usePathname();
 
-    const isTeacherPage = pathname?.startsWith("/teacher");
-    const isCoursePage = pathname?.includes("/courses");
-    const isSearchPage = pathname === '/search';
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isCoursePage = pathname?.includes("/courses");
+  const isSearchPage = pathname === "/search";
 
-    return (
-        <>
-        {isSearchPage && (
-            <div className="hidden md:block">
-                <SearchInput />
-            </div>
-        )}
-        <div className="flex justify-end gap-x-2 w-full">
-            {isTeacherPage || isCoursePage ? (
-                <Link href="/">
-                    <Button size="sm">
-                        <LogOut className="h-4 w-4 mr-2" />
-                        Exit
-                    </Button>
-                </Link>
-            ) : isTeacher(userId) ? (
-                <Link href="/teacher/courses">
-                    <Button size="sm">
-                        <UserIcon className="h-4 w-4 mr-2" />
-                        Teacher Mode
-                    </Button>
-                </Link>
-            ): null}
-            <UserButton 
-                afterSignOutUrl="/" 
-            />
+  return (
+    <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
         </div>
-        </>
-    );
-}
+      )}
+      <div className="flex justify-end gap-x-2 w-full">
+        {isTeacherPage || isCoursePage ? (
+          <Link href="/">
+            <Button size="sm">
+              <LogOut className="h-4 w-4 mr-2" />
+              Exit
+            </Button>
+          </Link>
+        ) : isTeacher(userId) ? (
+          <Link href="/teacher/courses">
+            <Button size="sm">
+              <UserIcon className="h-4 w-4 mr-2" />
+              Teacher Mode
+            </Button>
+          </Link>
+        ) : null}
+        <UserButton />
+      </div>
+    </>
+  );
+};
